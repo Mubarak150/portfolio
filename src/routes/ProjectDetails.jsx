2af634b163b61e74e7e2df5a1184b8e7231539ea
Mubarak150 <mkmubarak2347@gmail.com>
2024-05-28 09:05:53 -0700

    urls changed in project-details path accordingly2

--- src/routes/ProjectDetails.jsx
+++ src/routes/ProjectDetails.jsx
@@ -12,17 +12,19 @@ const ProjectDetails = () => {
     }
   
     return (
-      <div className="p-4">
+      <div className="p-4 flex flex-col relative h-[84vh]">
         <h1 className="text-2xl font-bold">{project.title}</h1>
         <div className="md:flex md:gap-7 text-justify">
-            <img src={'../public' + project.imageUrl.substring(1, project.imageUrl.length)} alt={project.title + "-landing-page"} className="w-full md:w-1/2 object-cover mt-4" />
+            <img src={project.imageUrl.substring(1, project.imageUrl.length)} alt={project.title + "-landing-page"} className="w-full md:w-1/2 object-cover mt-4" />
             <div className="md:flex md:flex-col md:justify-end ">
-                <p className="mt-4 text-primary font-teachers">{project.description}</p>
+                <p className="mt-4 text-primary text-lg font-teachers">{project.description}</p>
                 <p className="mt-8 text-primary font-teachers">Visit the <span className="underline hover:font-semibold cursor-pointer"><a href={project.gitLink} className="">Git Hub Repository</a></span> to learn more about it.</p>
             </div>
         </div>
+        <div className="absolute bottom-0 w-[97vw] m-0 p-0">
+           <Footer /> 
+        </div>
         
-        <Footer />
       </div>
     );
   };
